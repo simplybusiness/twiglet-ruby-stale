23503768b97634a690bafd337170e4c216523e82
Karan Shah <karan.shah@simplybusiness.co.uk>
2020-06-10 12:17:20 +0100

    Refactor to add_stack_trace

--- lib/twiglet/logger.rb
+++ lib/twiglet/logger.rb
@@ -40,14 +40,13 @@ module Twiglet
 
     def error(message, error = nil)
       if error
-        message = message.merge(
-          {
-            'error': {
-              'message': error.message,
-              'stack_trace': (error.backtrace.join("\n") if error.backtrace)
-            }.compact
+        error_fields = {
+          'error': {
+            'message': error.message
           }
-        )
+        }
+        add_stack_trace(error_fields, error)
+        message = message.merge(error_fields)
       end
 
       log(level: 'error', message: message)
@@ -88,6 +87,10 @@ module Twiglet
                      .deep_merge(@default_properties.to_nested)
                      .deep_merge(message.to_nested)
                      .to_json
+    end
+
+    def add_stack_trace(hash_to_add_to, error)
+      hash_to_add_to[:error][:stack_trace] = error.backtrace.join("\n") if error.backtrace
     end
   end
 end
